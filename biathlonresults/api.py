5b70e04203bfb42718d8480ab8c32f9ac053d3e5
prtkv <ip.tspl@gmail.com>
2024-12-24 23:48:04 +0100

    added new methods, typing, fixes

--- biathlonresults/api.py
+++ biathlonresults/api.py
@@ -1,127 +1,111 @@
+from typing import Any, Optional, Union
+
 import requests
 
-from .consts import LevelType
+from .consts import AnalysisType, LevelType
 
 ROOT_API = "http://biathlonresults.com/modules/sportapi/api/"
 
+ApiResponse = Union[dict[str, Any], list]
 
-def _request(method, params):
+
+def _request(method: str, params: Optional[dict[str, str]] = None) -> ApiResponse:
     try:
         return requests.get(ROOT_API + method, params=params).json()
     except requests.exceptions.RequestException as e:
         print("Something went wrong", e)
+        raise
 
 
-def cups(season_id):
-    """
-    List of Cups
-    :param season_id: season identifier (1819 for season 2018/2019, get others in a similar way)
-    :return: list of cups
-    :rtype: list
-    """
-    return _request("Cups", {"SeasonId": season_id})
+def organizers() -> ApiResponse:
+    return _request("Organizers")
 
 
-def cup_results(cup_id):
-    """
-    Cup results
-    BT1819SWRLCP__SMTS - Men's WC Total 2018/2019
-    BT1819SWRLCP__SWTS - Women's WC Total 2018/2019
-    :param cup_id: cup identifier, get it by calling cups(season_id)
-    :return: selected cup results
-    :rtype: dict
-    """
-    return _request("CupResults", {"CupId": cup_id})
+def seasons() -> ApiResponse:
+    return _request("Seasons")
 
 
-def athletes(family_name="", given_name=""):
-    """
-    Search athlete
-    :param family_name:
-    :param given_name:
-    :return: found athletes
-    :rtype: dict
-    """
-    return _request("Athletes", {"FamilyName": family_name, "GivenName": given_name})
-
-
-def cisbios(ibu_id):
-    """
-    Get athlete by id
-    :param ibu_id: ibu identifier of athlete
-    :return: athlete info
-    :rtype: dict
-    """
-    return _request("CISBios", {"IBUId": ibu_id})
-
-
-def all_results(ibu_id):
-    """
-    All races results of the athlete
-    :param ibu_id: ibu identifier of athlete
-    :return: races results
-    :rtype: dict
-    """
-    return _request("AllResults", {"IBUId": ibu_id})
-
-
-def events(season_id, level=LevelType.ALL):
+def events(season_id: str, level: Union[LevelType, str] = LevelType.ALL) -> ApiResponse:
     """
     Events of cup (schedule)
-    :param season_id: season identifier (1819 for season 2018/2019, get others in a similar way)
-    :param level: 0 - all, 1 - BMW IBU WC, 2 - IBU Cup, 3 - Jr. Cup
-    :return: list of events
-    :rtype: list
     """
     if isinstance(level, LevelType):
-        level = level.value
+        level = str(level.value)
     return _request("Events", {"SeasonId": season_id, "Level": level})
 
 
-def competitions(event_id):
+def competitions(event_id: str) -> ApiResponse:
     """
     List of races in stage
-    :param event_id: event identifier, get it by calling events(season_id, level=0)
-    :return: list of races
-    :rtype: list
     """
     return _request("Competitions", {"EventId": event_id})
 
 
-def results(race_id):
+def cups(season_id: str) -> ApiResponse:
+    """
+    List of Cups
+    season_id: season identifier (1819 for season 2018/2019, get others in a similar way)
+    """
+    return _request("Cups", {"SeasonId": season_id})
+
+
+def cup_results(cup_id: str) -> ApiResponse:
+    """
+    Cup results
+    BT1819SWRLCP__SMTS - Men's WC Total 2018/2019
+    BT1819SWRLCP__SWTS - Women's WC Total 2018/2019
+    """
+    return _request("CupResults", {"CupId": cup_id})
+
+
+def athletes(family_name: str = "", given_name: str = "") -> ApiResponse:
+    """
+    Search athlete
+    """
+    return _request("Athletes", {"FamilyName": family_name, "GivenName": given_name})
+
+
+def cisbios(ibu_id: str) -> ApiResponse:
+    """
+    Get athlete by id
+    """
+    return _request("CISBios", {"IBUId": ibu_id})
+
+
+def all_results(ibu_id: str) -> ApiResponse:
+    """
+    All races results of the athlete
+    """
+    return _request("AllResults", {"IBUId": ibu_id})
+
+
+def results(race_id: str) -> ApiResponse:
     """
     Race results
-    :param race_id: race identifier, get it by calling competitions(event_id)
-    :return: list of races
-    :rtype: dict
     """
     return _request("Results", {"RaceId": race_id})
 
 
+def analytic_results(race_id: str, type_id: Union[AnalysisType, str]) -> ApiResponse:
+    if isinstance(type_id, AnalysisType):
+        type_id = type_id.value
+    return _request("AnalyticResults", {"RaceId": race_id, "TypeId": type_id})
+
+
 def stats(
-    statistic_id,
-    stat_id,
-    by_what,
-    gender_id,
-    season_id="",
-    organizer_id="",
-    ibu_id="",
-    nat="",
-):
+    statistic_id: str,
+    stat_id: str,
+    by_what: str,
+    gender_id: str,
+    season_id: str = "",
+    organizer_id: str = "",
+    ibu_id: str = "",
+    nat: str = "",
+) -> ApiResponse:
     """
     Statistics
     Not sure how to use it right, here's default query parameters:
     StatisticId=WCPOD_M&StatId=WCPOD&byWhat=ATH&SeasonId=&OrganizerId=&GenderId=M&IBUId=&Nat=
-    :param statistic_id:
-    :param stat_id:
-    :param by_what:
-    :param gender_id:
-    :param season_id:
-    :param organizer_id:
-    :param ibu_id:
-    :param nat:
-    :return: statistics
-    :rtype: dict
     """
     return _request(
         "Stats",
--- biathlonresults/consts.py
+++ biathlonresults/consts.py
@@ -1,8 +1,62 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 
-class LevelType(Enum):
-    ALL = 0
-    BMW_IBU_WC = 1
+class LevelType(IntEnum):
+    ALL_LEVELS = -1  # includes all levels (1-6)
+    ALL = 0  # includes levels 1-4
+    BMW_IBU_WC = 1  # world cup, that's what you need
     IBU_CUP = 2
     JR_CUP = 3
+    OTH = 4
+    REG = 5
+    PARA = 6
+
+
+class AnalysisType(Enum):
+    TOTAL_COURSE_TIME = "CRST"
+    TOTAL_RANGE_TIME = "RNGT"
+    TOTAL_SHOOTING_TIME = "STTM"
+    SKI_TIME = "SKIT"
+    PURSUIT_TIME = "PURS"
+    BEST_WOMEN = "BSTW"
+    BEST_MEN = "BSTM"
+    RESULTS_LEG_1 = "FI1L"
+    RESULTS_LEG_2 = "FI2L"
+    RESULTS_LEG_3 = "FI3L"
+    RESULTS_LEG_4 = "FI4L"
+    COURSE_TIME_LEG_1 = "CRST1"
+    COURSE_TIME_LEG_2 = "CRST2"
+    COURSE_TIME_LEG_3 = "CRST3"
+    COURSE_TIME_LEG_4 = "CRST4"
+    RANGE_TIME_LEG_1 = "RNGT1"
+    RANGE_TIME_LEG_2 = "RNGT2"
+    RANGE_TIME_LEG_3 = "RNGT3"
+    RANGE_TIME_LEG_4 = "RNGT4"
+    COURSE_TIME_LAP_1 = "CRS1"
+    COURSE_TIME_LAP_2 = "CRS2"
+    COURSE_TIME_LAP_3 = "CRS3"
+    COURSE_TIME_LAP_4 = "CRS4"
+    COURSE_TIME_LAP_5 = "CRS5"
+    COURSE_TIME_LAP_6 = "CRS6"
+    COURSE_TIME_LAP_7 = "CRS7"
+    COURSE_TIME_LAP_8 = "CRS8"
+    COURSE_TIME_LAP_9 = "CRS9"
+    COURSE_TIME_LAP_10 = "CRS10"
+    COURSE_TIME_LAP_11 = "CRS11"
+    COURSE_TIME_LAP_12 = "CRS12"
+    RANGE_TIME_1 = "RNG1"
+    RANGE_TIME_2 = "RNG2"
+    RANGE_TIME_3 = "RNG3"
+    RANGE_TIME_4 = "RNG4"
+    RANGE_TIME_5 = "RNG5"
+    RANGE_TIME_6 = "RNG6"
+    RANGE_TIME_7 = "RNG7"
+    RANGE_TIME_8 = "RNG8"
+    SHOOTING_TIME_1 = "S1TM"
+    SHOOTING_TIME_2 = "S2TM"
+    SHOOTING_TIME_3 = "S3TM"
+    SHOOTING_TIME_4 = "S4TM"
+    SHOOTING_TIME_5 = "S5TM"
+    SHOOTING_TIME_6 = "S6TM"
+    SHOOTING_TIME_7 = "S7TM"
+    SHOOTING_TIME_8 = "S8TM"
